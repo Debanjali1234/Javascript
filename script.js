45a19b8ebd9924a772855d1ba6d59a8dd8814c6b
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-04-30 01:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//console.log("hello debanjali");
+
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+let c=null;
+console.log(typeof c);
+let b;
+console.log(typeof b);
+b=10000;
+console.log(typeof b);
+console.log(typeof "b");
