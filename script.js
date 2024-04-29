1f45925ce6bca8dcc115345595dda6b9da5bc3fa
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-04-30 02:15:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-//console.log("hello debanjali");
+let a=100;
+let b=100;
 
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof undefined);
-let c=null;
-console.log(typeof c);
-let b;
-console.log(typeof b);
-b=10000;
-console.log(typeof b);
-console.log(typeof "b");
+console.log("the value is",a+b);
+console.log("the value is",a-b);
+console.log("the value is",a*b);
+console.log("the value is",a/b);
+console.log("the value is",a%b);
+
+b=100;
+console.log("the value of a",a);//100
+console.log("the value of b",b);//100
+
+console.log("the value of a after a++",a++);//post incremenet//100
+console.log("the value of a in nextline",a);//101
+console.log("the value of b after ++b",++b);//101//pre increment
+console.log("the value of b in next line",b);//101
+
+console.log("the value of a after a--",a--);//post decrement//101
+console.log("the value of a in nextline",a);//100
+console.log("the value of b after --b", --b);//100//pre decrement
+console.log("the value of b in next line", b);//100
