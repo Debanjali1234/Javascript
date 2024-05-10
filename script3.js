3de637189656d8ae4318c89411ec34e44bda35b0
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-05-10 19:45:46 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,23 @@
+let even_sum=[];
+let odd_num=[];
+for(let i=0;i<=20;i++){
+    if (i % 2 == 0)
+        {
+
+        even_sum.push(i);
+
+        }
+
+        else
+
+        {
+
+            odd_num.push(i);
+
+        }
+
+    }
+    
+    console.log(even_sum);
+    
+    console.log(odd_num);
