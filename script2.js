7a84b5e613b88b7ea161d35597e94ee153a3f1fe
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-05-10 12:42:11 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+// array in js:-
+var num=[1,2,3];
+console.log(num);
+num.push(4);
+console.log(num);
+num.unshift(5);
+console.log(num);
+num[0]=100;  // change index value
+console.log(num);
+console.log(num.length);
+
+
