7db304e44b4bed107b8cf8452355d5b479496fdc
DEBANJALI BHATTACHARYA <126330453+Debanjali1234@users.noreply.github.com>
2024-05-10 20:11:20 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,23 @@
+let even_sum=[];
+let odd_num=[];
+for(let i=0;i<=100;i++){
+    if (i % 2 === "0")// here checking the type is same or not!
+        {
+
+        even_sum.push("the even num.is->",i);
+
+        }
+
+        else
+
+        {
+
+            odd_num.push("the odd num.is->",i);
+
+        }
+
+    }
+    
+    console.log(even_sum);
+    
+    console.log(odd_num);
